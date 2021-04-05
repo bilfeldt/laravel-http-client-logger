fbaede39699017345df60ade0b4e82d701108bd3
Anders Bilfeldt <abilfeldt@gmail.com>
2021-04-05 22:58:53 +0200

    Implement PsrMessage conversion helper methods

--- src/PsrMessageToStringConverter.php
+++ src/PsrMessageToStringConverter.php
@@ -3,6 +3,8 @@
 namespace Bilfeldt\LaravelHttpClientLogger;
 
 use GuzzleHttp\Psr7\Message;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\MessageInterface;
 
 class PsrMessageToStringConverter
@@ -10,5 +12,15 @@ class PsrMessageToStringConverter
     public function toString(MessageInterface $message, array $placeholders): string
     {
         return strtr(Message::toString($message), $placeholders);
+    }
+
+    public function toRequest(string $message): Request
+    {
+        return Message::parseRequest($message);
+    }
+
+    public function toResponse(string $message): Response
+    {
+        return Message::parseResponse($message);
     }
 }

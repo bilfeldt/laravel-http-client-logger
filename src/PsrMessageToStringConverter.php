31b3fe8c33d74da01f89db0f69fd4f820b2631ee
basteln3rk <gnutzmann@flexponsive.net>
2024-10-02 07:31:23 +0000

    add message accessor singleton; use message accessor in the message to string converter

--- src/PsrMessageToStringConverter.php
+++ src/PsrMessageToStringConverter.php
@@ -9,9 +9,17 @@ use Psr\Http\Message\MessageInterface;
 
 class PsrMessageToStringConverter
 {
+    protected MessageAccessor $messageAccessor;
+
+    public function __construct(MessageAccessor $messageAccessor)
+    {
+        $this->messageAccessor = $messageAccessor;
+    }
+
     public function toString(MessageInterface $message, array $replace): string
     {
-        return strtr(Message::toString($message), $replace);
+        $filteredMessage = $this->messageAccessor->filterMessage($message);
+        return strtr(Message::toString($filteredMessage), $replace);
     }
 
     public function toRequest(string $message): Request
